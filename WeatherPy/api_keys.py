1a9c5aef2850e5f4c06c1ce056e7014d821f44bf
SteliosKosmidis <125541671+SteliosKosmidis@users.noreply.github.com>
2023-05-17 18:14:45 -0400

    Add files via upload

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "77a1b55fe8ca0310457bae0c85fcf767"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "04d594a52dcc4614bd562fcadca136e1"
